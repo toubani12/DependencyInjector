1b8b094b4b54256310a0e0f090696d07f9e0ea89
badr <fadre6@gmail.com>
2026-02-18 17:48:01 +0000

    feat:test cMetier

--- src/main/java/com/badhacker/pres/Pres2.java
+++ src/main/java/com/badhacker/pres/Pres2.java
@@ -1,6 +1,7 @@
 package com.badhacker.pres;
 
 import com.badhacker.dao.IDao;
+import com.badhacker.metier.IMetier;
 import com.badhacker.metier.impl.MetierImpl;
 
 import java.io.File;
@@ -8,14 +9,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main (String[] args) throws FileNotFoundException , ClassNotFoundException ,InstantiationException, IllegalAccessException {
+    //FileNotFoundException , ClassNotFoundException ,InstantiationException, IllegalAccessException
+    public static void main (String[] args) throws Exception {
         Scanner sc = new Scanner(new File("config.txt"));
         String daoClassName = sc.nextLine();
         String metierClassName = sc.nextLine();
         Class cDao = Class.forName(daoClassName);
+        Class cMetier = Class.forName(metierClassName);
         IDao dao = (IDao) cDao.newInstance();
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
 
-        System.out.println(dao.getData());
+        System.out.println("Res : " + metier.calcul() + "kelvin");
 
 
     }
