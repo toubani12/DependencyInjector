771569c297419f80f33b60c9421b0d68669f2dbc
badr <fadre6@gmail.com>
2026-02-18 17:41:01 +0000

    feat:create Pres 2 and use config.txt

--- src/main/java/com/badhacker/pres/Pres2.java
+++ src/main/java/com/badhacker/pres/Pres2.java
@@ -1,7 +1,22 @@
 package com.badhacker.pres;
 
+import com.badhacker.dao.IDao;
+import com.badhacker.metier.impl.MetierImpl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Pres2 {
-    public static void main (String[] args){
+    public static void main (String[] args) throws FileNotFoundException , ClassNotFoundException ,InstantiationException, IllegalAccessException {
+        Scanner sc = new Scanner(new File("config.txt"));
+        String daoClassName = sc.nextLine();
+        String metierClassName = sc.nextLine();
+        Class cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.newInstance();
+
+        System.out.println(dao.getData());
+
 
     }
 }
